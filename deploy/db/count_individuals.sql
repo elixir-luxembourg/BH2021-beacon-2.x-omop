065f31de47bc7e2f9d549235dc1ef57ac087c41d
Maxim Moinat <maxim@thehyve.nl>
2021-11-10 14:04:58 +0100

    add parameter gender concept id

--- deploy/db/count_individuals.sql
+++ deploy/db/count_individuals.sql
@@ -3,9 +3,10 @@ DROP FUNCTION IF EXISTS public.count_individuals;
 CREATE FUNCTION public.count_individuals(
     _disease_code text,
     _disease_code_vocabulary text,
-    _min_age_of_onset integer,
-    _max_age_of_onset integer,
-    _include_descendants bool
+    _min_age_of_onset integer default NULL,
+    _max_age_of_onset integer default NULL,
+    _include_descendants bool default NULL,
+    _gender_concept_id integer default NULL  -- 8507 for male, 8532 for female
     )
     RETURNS TABLE(_num_total_results bigint)
     LANGUAGE plpgsql
@@ -32,6 +33,7 @@ BEGIN
     END IF;
 
     -- #1=_disease_code, #2=_disease_vocabulary, #3=_min_age_of_onset, #4=_max_age_of_onset
+    -- #5=_gender_concept_id
     _where_clause = '
 	WHERE ancestor.concept_code = $1 AND ancestor.vocabulary_id = $2';
 
@@ -50,12 +52,16 @@ BEGIN
 		AND min_levels_of_separation = 0'; -- only match on given code
     END IF;
 
+    IF _gender_concept_id IS NOT NULL THEN
+        _where_clause = _where_clause || 'AND gender_concept_id = $5';
+    END IF;
+
     _query = _query || _where_clause;
 
     RAISE NOTICE '_query: %', _query;
 
     RETURN QUERY EXECUTE _query
-        USING _disease_code, _disease_code_vocabulary, _min_age_of_onset, _max_age_of_onset;
+        USING _disease_code, _disease_code_vocabulary, _min_age_of_onset, _max_age_of_onset, _gender_concept_id;
 END
 $$;
 
@@ -63,4 +69,5 @@ $$;
 -- * ~Build query in sqlalchemy (orm)~
 -- * Make a generic count_individuals, independent of domain
 -- * Accept multiple disease codes.
--- * age of onset: for the first time in someones history.
+-- * age of onset: for the first time in someones history. => solved with era
+-- * write out doc which describes how beacon request is translated into OMOP query. e.g. diseases from era
